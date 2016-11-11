cfb91fe4d861d5fbc0c444164a2d25d239eeaa36
Zavisa Nikac <znikac@srce.hr>
2016-11-11 23:36:12 +0100

    lint fixes

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,20 +1,20 @@
 class monit::config  inherits monit {
 
-  file { "$monit::monitconfd_dir": 
-    ensure  => directory, 
-    recurse => true, 
-    purge   => true, 
-  } 
+  file { $monit::monitconfd_dir:
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+  }
 
-  file { "$monit::monitrc_file":
+  file { $monit::monitrc_file:
     ensure  => file,
-    content => epp("$monit::monitrc_epp"),
+    content => epp($monit::monitrc_epp),
     owner   => $monit::file_owner,
     group   => $monit::file_group,
     mode    => $monit::monitrc_mode,
   }
 
-  file { "$monit::rootbin_file":
+  file { $monit::rootbin_file:
     ensure => file,
     source => $monit::rootbin_src,
     owner  => $monit::file_owner,
@@ -22,7 +22,7 @@ class monit::config  inherits monit {
     mode   => $monit::file_mode,
   }
 
-  file { "$monit::rootrc_file":
+  file { $monit::rootrc_file:
     ensure => file,
     source => $monit::rootrc_src,
     owner  => $monit::file_owner,
@@ -30,7 +30,7 @@ class monit::config  inherits monit {
     mode   => $monit::file_mode,
   }
 
-  file { "$monit::rootstrict_file":
+  file { $monit::rootstrict_file:
     ensure => file,
     source => $monit::rootstricet_src,
     owner  => $monit::file_owner,
@@ -39,7 +39,7 @@ class monit::config  inherits monit {
   }
 
   $monit::services_managed.each |$manage| {
-    monit::services { "$manage": }
+    monit::services { $manage: }
   }
 
 }
